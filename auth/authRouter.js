3a1e9f8ae8b0b8515f92743f8fb8068561794a24
Dan Sherman <dvsherman@protonmail.com>
2019-04-04 23:03:30 -0700

    added dotenv, created .env, set up tokenService.js, building authRouter.js

--- auth/authRouter.js
+++ auth/authRouter.js
@@ -1,2 +1,28 @@
 const router = require("express").Router();
 const bcrypt = require("bcryptjs");
+
+const tokenService = require('./tokenService');
+const uModel = require('../users/usersModel');
+
+router.post('/register', async (req, res) => {
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 5);
+  user.password = hash;
+
+  try {
+    const newUser = uModel.add(user);
+    res.status(201).json(newUser);
+  } catch (error) {
+    res.status(500).json(error);
+  };
+});
+
+router.post('/login', async (req, res) => {
+  let { username, password } = req.body;
+
+  try {
+    const 
+  } catch (error) {
+    
+  }
+})
