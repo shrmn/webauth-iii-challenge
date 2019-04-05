804baf50b59e26cc57be9aee068364ba38a93536
Dan Sherman <dvsherman@protonmail.com>
2019-04-05 00:57:05 -0700

    set up Users.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,33 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Route, NavLink } from "react-router-dom";
 import "./App.css";
 
 import Login from "./components/Login";
+import Users from "./components/Users";
 
 const App = () => {
   return (
-    <div>
+    <>
+      <header>
+        <NavLink to="/">Home</NavLink>
+        &nbsp;|&nbsp;
+        <NavLink to="/login">Login</NavLink>
+        &nbsp;|&nbsp;
+        <NavLink to="/users">Users</NavLink>
+      </header>
       <Route exact path="/" component={Home} />
       <Route path="/login" component={Login} />
-    </div>
+      <Route path="/users" component={Users} />
+    </>
   );
 };
 
 const Home = () => {
   return <h1>What a day! What a lovely day!</h1>;
+};
+
+const Logout = () => {
+  localStorage.removeItem("token");
 };
 
 export default App;
